151f7ead04735866e0afaedee83e512460d9aa51
Per Jahn <perjahn@gmail.com>
2021-07-14 22:19:00 +0200

    1st

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc myprogram.cpp -o myprogram -lglfw
---
+++ myprogram.cpp
@@ -0,0 +1,22 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "MyProgram", NULL, NULL);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
